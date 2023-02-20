895660ced9afc886fd182cc7a96f30c449491eaa
윤건수 <ygs3004@naver.com>
2023-02-20 11:29:15 +0900

    230220 프로그래머스 sql

---
+++ programmers-quiz/places.sql
@@ -0,0 +1,36 @@
+PLACES 테이블은 공간 임대 서비스에 등록된 공간의 정보를 담은 테이블입니다. PLACES 테이블의 구조는 다음과 같으며 
+ID, NAME, HOST_ID는 각각 공간의 아이디, 이름, 공간을 소유한 유저의 아이디를 나타냅니다. ID는 기본키입니다.
+
+NAME  TYPE
+ID  INT
+NAME  VARCHAR
+HOST_ID INT
+
+
+CREATE TABLE IF NOT EXISTS PLACES (
+  ID INT PRIMARY KEY,
+  NAME VARCHAR(50),
+  HOST_ID INT
+);
+
+INSERT INTO PLACES (ID, NAME, HOST_ID) VALUES (4431977, 'BOUTIQUE STAYS - Somerset Terrace, Pet Friendly', 760849);
+INSERT INTO PLACES (ID, NAME, HOST_ID) VALUES (5194998, 'BOUTIQUE STAYS - Elwood Beaches 3, Pet Friendly', 760849);
+INSERT INTO PLACES (ID, NAME, HOST_ID) VALUES (16045624, 'Urban Jungle in the Heart of Melbourne', 30900122);
+INSERT INTO PLACES (ID, NAME, HOST_ID) VALUES (17810814, 'Stylish Bayside Retreat with a Luscious Garden', 760849);
+INSERT INTO PLACES (ID, NAME, HOST_ID) VALUES (22740286, 'FREE PARKING - The Velvet Lux in Melbourne CBD', 30900122);
+INSERT INTO PLACES (ID, NAME, HOST_ID) VALUES (22868779, '★ Fresh Fitzroy Pad with City Views! ★', 21058208);
+
+-- 테이블 확인
+SELECT * FROM PLACES;
+
+-- 데이터 비우기
+TRUNCATE PLACES;
+
+-- 이 서비스에서는 공간을 둘 이상 등록한 사람을 "헤비 유저"라고 부릅니다. 헤비 유저가 등록한 공간의 정보를 아이디 순으로 조회하는 SQL문을 작성해주세요.
+SELECT *
+  FROM PLACES
+ WHERE HOST_ID IN (SELECT HOST_ID
+                     FROM PLACES
+                    GROUP BY HOST_ID
+                   HAVING COUNT(*) > 1)
+ ORDER BY ID;
--- programmers-quiz/product.sql
+++ programmers-quiz/product.sql
@@ -68,5 +68,15 @@ SELECT PRODUCT_CODE
   FROM PRODUCT P
   JOIN OFFLINE_SALE OS ON P.PRODUCT_ID = OS.PRODUCT_ID
  GROUP BY P.PRODUCT_CODE
- ORDER BY;
+ ORDER BY SALES DESC, PRODUCT_CODE;
  
+-- RODUCT 테이블에서 만원 단위의 가격대 별로 상품 개수를 출력하는 SQL 문을 작성해주세요. 
+-- 때 컬럼명은 각각 컬럼명은 PRICE_GROUP, PRODUCTS로 지정해주시고 가격대 정보는 각 구간의 최소금액(10,000원 이상 ~ 20,000 미만인 구간인 경우 10,000)으로 
+-- 표시해주세요. 결과는 가격대를 기준으로 오름차순 정렬해주세요.
+SELECT TRUNCATE(PRICE, -4) AS PRICE_GROUP
+     , COUNT(*) PRODUCTS
+  FROM PRODUCT
+ GROUP BY PRICE_GROUP
+ ORDER BY PRICE_GROUP;
+ 
+ HAVING BETWEEN 
